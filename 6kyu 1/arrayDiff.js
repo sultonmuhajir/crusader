eb65499e93f20b5f2eecae291b319c21c94909fc
Sulton Muhajir <sulton.muhajir28@gmail.com>
2021-06-28 12:35:03 +0700

    6.11

---
+++ 6kyu 1/arrayDiff.js
@@ -0,0 +1,24 @@
+function arrayDiff(a, b) {
+   for (let i = 0; i < b.length; i++) {
+      a = a.filter(x => x != b[i])
+   }
+   return a;
+}
+
+
+function arrayDiff(a, b) {
+   return a.filter(e => !b.includes(e));
+}
+
+
+function arrayDiff(a, b) {
+   return a.filter(function (x) {
+      return b.indexOf(x) == -1;
+   });
+}
+
+
+console.log(arrayDiff([], [4, 5]), []);
+console.log(arrayDiff([3, 4], [3]), [4]);
+console.log(arrayDiff([1, 8, 2], []), [1, 8, 2]);
+console.log(arrayDiff([1, 2, 3], [1, 2]), [3])
