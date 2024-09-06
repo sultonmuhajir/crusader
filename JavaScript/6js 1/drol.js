fc5ad9eb83f3890ce3a2c80b531487dca89a15c1
Sulton Muhajir <sulton.muhajir28@gmail.com>
2024-09-06 15:52:47 +0700

    93.92.92

---
+++ JavaScript/6js 1/drol.js
@@ -0,0 +1,21 @@
+/*
+|--------------------------------------------------------------------------
+| Don't rely on luck.
+|--------------------------------------------------------------------------
+|
+*/
+
+var guess = 10;
+Math.floor = function () {
+   return guess;
+};
+
+
+Math.random = function (n) {
+   return 0;
+};
+var guess = 1;
+
+
+Math.random = () => 0;
+var guess = 1;
